5f8499db3066f73255163801d47f4c22bc90576f
zh3ngxu <xanderxu1994@gmail.com>
2024-01-20 20:14:16 +0000

    add hostname comment and update es query dsl

--- backend_api/main.py
+++ backend_api/main.py
@@ -4,17 +4,19 @@ from elasticsearch import Elasticsearch
 
 app = FastAPI()
 
-# Elasticsearch connection
+# Elasticsearch connection use localhost if testing locally
+# hostname elasticsearch can be parsed in Docker
+# es = Elasticsearch(hosts=[{"host": "localhost", "port": 9200}])
 es = Elasticsearch(hosts=[{"host": "elasticsearch", "port": 9200}])
 
 @app.get("/")
 async def read_root():
-    return {"message": "Hello, this is your FastAPI app!"}
+    return {"message": "Hello, this is your FastAPI app! See /docs for more details"}
 
 @app.get("/search/{index}")
 async def search_index(index: str, query: str):
     try:
-        result = es.search(index=index, body={"query": {"match": {"content": query}}})
+        result = es.search(index=index, body={"query": {"match": {"article_content_first_4_graph": query}}})
         return JSONResponse(content=result, status_code=200)
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
